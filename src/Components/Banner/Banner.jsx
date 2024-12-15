20fb0908d5e4c21faf6d6f60b3a7ea9a315c4af2
Mohammad Ayub <5043ayub@gmail.com>
2024-12-15 14:45:19 +0600

    Error page added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,12 +3,12 @@ import banner from '../../assets/bookimg/banner.png'
 const Banner = () => {
   return (
     <div>
-        <div className='bg-third flex lg:flex-row md:flex-row sm:flex-col max-sm:flex-col justify-center items-center lg:px-20 md:px-12 sm:px-8 max-sm:px-4 lg:py-12 md:py-8 sm:py-6 max-sm:py-6 rounded-3xl'>
-            <div className='lg:w-1/2 md:w-1/2 sm:w-4/5 max-sm:w-4/5 lg:text-start md:text-start sm:text-center max-sm:text-center lg:mb-0 md:mb-0 sm:mb-6 max-sm:mb-6'>
+        <div className='bg-third flex lg:flex-row md:flex-row sm:flex-col-reverse max-sm:flex-col-reverse justify-center items-center lg:px-20 md:px-12 sm:px-8 max-sm:px-4 lg:py-12 md:py-8 sm:py-6 max-sm:py-6 rounded-3xl'>
+            <div className='lg:w-1/2 md:w-1/2 sm:w-4/5 max-sm:w-4/5 lg:text-start md:text-start sm:text-center max-sm:text-center'>
                 <h1 className='font-playfair lg:text-6xl md:text-4xl sm:text-2xl max-sm:text-2xl leading-tight lg:mb-10 md:10 sm:mb-5 max-sm:mb-5 font-semibold'>Books to freshen up your bookshelf</h1>
-                <Link to='/listedbooks' className='bg-main text-white px-6 py-2 rounded-md font-bold' to='/'>View The List</Link>
+                <Link to='/listedbooks' className='bg-main text-white px-6 py-2 rounded-md font-bold'>View The List</Link>
             </div>
-            <div className='lg:w-1/2 md:w-1/2 sm:w-full max-sm:w-full flex lg:justify-end md:justify-end sm:justify-center max-sm:justify-center'>
+            <div className='lg:w-1/2 md:w-1/2 sm:w-full max-sm:w-full flex lg:justify-end md:justify-end sm:justify-center max-sm:justify-center lg:mb-0 md:mb-0 sm:mb-6 max-sm:mb-6'>
                 <img className='sm:w-2/3 max-sm:w-2/3' src={banner} alt="a book picture" />
             </div>
         </div>
--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { TfiFaceSad } from 'react-icons/tfi'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
     <div>
-        <Header></Header>
-        <div className='lg:px-20 md:px-8 sm:px-6 max-sm:px-4 py-36'>
+        <div className='lg:px-20 md:px-8 sm:px-6 max-sm:px-4 lg:py-36 md:py-36 sm:py-32 max-sm:py-28'>
           <div className='flex flex-col justify-center items-center'>
             <TfiFaceSad className='text-8xl font-bold mb-3'></TfiFaceSad>
             <p className='font-bold text-6xl mb-3'>404</p>
+            <p className='font-bold text-4xl mb-3 text-neutral-400'>Page not found</p>
+            <p className='font-semibold text-neutral-700 text-center'>The page you are looking for doesn't exist or another error occured.</p>
+            <p className='font-semibold text-neutral-700 text-center'>Go back, or head over the <Link to='/'>bookvibe.com</Link> to choose a new direction.</p>
           </div>
         </div>
-        <Footer></Footer>
     </div>
   )
 }
